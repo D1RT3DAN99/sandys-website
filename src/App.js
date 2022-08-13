dedd623963149d0f11488ab7cd46d39b23c676d9
Daniel Ramirez <dramirtek99@gmail.com>
2022-08-13 11:43:29 -0700

    skeleton code updated and functional for the contact us and homepage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Homepage from './Homepage';
+import Contact from './Contact';
 
-import{BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage/>} />
+          <Route path="/contactus" element={<Contact/>} />
+        </Routes>
       </div>
     </Router>
   );
